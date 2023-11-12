4d3fb67da64eba3db1c5792398a3b7714c9a4fd9
YurySkosyrev <skosyrev-87@mail.ru>
2023-11-12 22:15:46 +0300

    Add RedisConfiguration with RedisTemplate

---
+++ src/main/java/com/example/general/links/RedisConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.general.links;
+
+import com.example.general.common.ShortLink;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+
+    RedisTemplate<String, ShortLink> template(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String, ShortLink> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+}
